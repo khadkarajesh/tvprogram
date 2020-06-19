0bbdd4cf4ddab7bf435f164691fdc9cb415e949c
rajesh <rajesh.k.khadka@gmail.com>
2020-06-19 22:13:34 +0545

    add back navigation

--- app/src/main/java/com/nepninja/tvprogram/MainActivity.kt
+++ app/src/main/java/com/nepninja/tvprogram/MainActivity.kt
@@ -2,10 +2,25 @@ package com.nepninja.tvprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //https://stackoverflow.com/questions/58703451/fragmentcontainerview-as-navhostfragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
