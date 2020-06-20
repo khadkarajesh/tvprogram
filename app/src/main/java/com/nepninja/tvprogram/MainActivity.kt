518948bbf4c4feb60bced44b76587c2342a08c56
rajesh <rajesh.k.khadka@gmail.com>
2020-06-20 11:13:51 +0545

    remove comment

--- app/src/main/java/com/nepninja/tvprogram/MainActivity.kt
+++ app/src/main/java/com/nepninja/tvprogram/MainActivity.kt
@@ -8,12 +8,11 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //https://stackoverflow.com/questions/58703451/fragmentcontainerview-as-navhostfragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
